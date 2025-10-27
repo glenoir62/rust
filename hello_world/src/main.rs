407f6a85f139615d00341fb7160ead053f06e1cb
Guillaume Lenoir <guillaume.lenoir@ext.adeo.com>
2025-10-27 16:57:19 +0100

    feat(trait): init trait

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world,test!");
+    let x:i32 = 33;
+    x.afficher();
+    println!("C'est un test : {}", x);
+
+}
+
+// Définition du trait Affichable
+trait Affichable {
+    fn afficher(&self);
+}
+
+// Implémentation du trait Affichable pour le type i32
+impl Affichable for i32 {
+    fn afficher(&self) {
+        println!("C'est un nombre : {}", self);
+    }
 }
